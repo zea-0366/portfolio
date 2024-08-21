fea7a390c47b582c47fc3762cc8fd9f6546edeba
Fariza Zea <farizazea9c@gmail.com>
2024-08-21 11:09:32 +0700

    ukuran foto

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -33,7 +33,6 @@ window.addEventListener("load", () => {
     dayNight.querySelector("i").classList.add("fa-sun");
   }
   else {
-
     dayNight.querySelector("i").classList.add("fa-moon");
   }
 })
