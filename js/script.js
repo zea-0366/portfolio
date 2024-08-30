b5e0d14d9c59f1b89b3ddb8e4a1e96a98e72764a
Fariza Zea <farizazea9c@gmail.com>
2024-08-30 17:27:50 +0700

    tombol home warna bergantian

--- js/script.js
+++ js/script.js
@@ -5,18 +5,34 @@ var typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true
 })
-// ============ aside =========
+// ============ aside baru=========
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
   totalNavList = navList.length;
-for (let i = 0; i < totalNavList; i++) {
+for (let i = 0; i < totalNavList; i++) 
+  {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
+    for(let j = 0; j<totalNavList; j++) {
+      navList[j].querySelector("a").classList.remove("active");
+    }
     this.classList.add("active")
   })
 }
-// toggler
 
-// AKTIVASI FORM EMAIL 
+
+
+
+// show elemen
+function showSection(element) 
+{
+  for(let i=0; i<totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+   const target = element.getAttribute("href").split("#")[1];
+   document.querySelector("#" ,target).classList.add("active")
+}
+
+
 
 
